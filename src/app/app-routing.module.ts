b6397793722840941d187b9dd361b5682f90df25
faykris <faykris28@gmail.com>
2023-09-09 14:29:08 -0500

    Add desktop landing page, pending header dropdowns

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { FeaturesComponent } from './pages/features/features.component';
+import { CompanyComponent } from './pages/company/company.component';
+import { CareersComponent } from './pages/careers/careers.component';
+import { AboutComponent } from './pages/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'features', component: FeaturesComponent },
+  { path: 'company', component: CompanyComponent },
+  { path: 'careers', component: CareersComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './modules/header/header.component';
+import { FooterComponent } from './modules/footer/footer.component';
+import { FeaturesComponent } from './pages/features/features.component';
+import { CompanyComponent } from './pages/company/company.component';
+import { CareersComponent } from './pages/careers/careers.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FeaturesComponent,
+    CompanyComponent,
+    CareersComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
